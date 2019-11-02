f598874df813fc0268dce17d10284f9208c0a414
aias00 <rokkki@163.com>
2019-11-02 17:42:47 +0800

    删除无用代码

--- security-spring-boot-autoconfigure/src/main/java/com/aias/springboot/security/jwt/JwtTokenUtil.java
+++ security-spring-boot-autoconfigure/src/main/java/com/aias/springboot/security/jwt/JwtTokenUtil.java
@@ -1,168 +1,174 @@
 package com.aias.springboot.security.jwt;
 
-import com.aias.springboot.security.utils.DateUtils;
-import io.jsonwebtoken.*;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.annotation.PostConstruct;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import io.jsonwebtoken.*;
 
 @Component
 public class JwtTokenUtil {
 
-    private final Log logger = LogFactory.getLog(JwtTokenUtil.class);
-    // (主题)
-    private static final String CLAIM_KEY_USER_ID = "sub";
-    // (目标受众)
-    private static final String CLAIM_KEY_CREATED = "created";
+	private final Log logger = LogFactory.getLog(JwtTokenUtil.class);
+	// (主题)
+	private static final String CLAIM_KEY_USER_ID = "sub";
+	// (目标受众)
+	private static final String CLAIM_KEY_CREATED = "created";
 
-    public static final String ANONYMOUS_USER = "anonymousUser";
+	public static final String ANONYMOUS_USER = "anonymousUser";
 
-    private String secret;
+	private String secret;
 
-    private Integer expirationDay;
+	private Integer expirationDay;
 
-    public String headerKey;
+	public String headerKey;
 
-    public String getHeaderKey() {
-        return headerKey;
-    }
+	public String getHeaderKey() {
+		return headerKey;
+	}
 
-    @Autowired
-    private JwtProperties properties;
+	@Autowired
+	private JwtProperties properties;
 
-    @PostConstruct
-    public void init() {
-        this.secret = this.properties.getSecret();
-        this.expirationDay = this.properties.getExpiration();
-        this.headerKey = this.properties.getHeaderKey();
-    }
+	@PostConstruct
+	public void init() {
+		this.secret = this.properties.getSecret();
+		this.expirationDay = this.properties.getExpiration();
+		this.headerKey = this.properties.getHeaderKey();
+	}
 
+	public String getUsernameFromToken(String token) {
+		logger.info("secret = " + secret + ",  expirationDay = " + expirationDay
+				+ ",  token:" + token);
+		String username;
+		try {
+			final Claims claims = getClaimsFromToken(token);
+			username = claims.getSubject();
+		} catch (Exception e) {
+			username = null;
+		}
+		return username;
+	}
 
-    public String getUsernameFromToken(String token) {
-        logger.info("secret = " + secret + ",  expirationDay = " + expirationDay
-                + ",  token:" + token);
-        String username;
-        try {
-            final Claims claims = getClaimsFromToken(token);
-            username = claims.getSubject();
-        } catch (Exception e) {
-            username = null;
-        }
-        return username;
-    }
+	public Date getCreatedDateFromToken(String token) {
+		Date created;
+		try {
+			final Claims claims = getClaimsFromToken(token);
+			created = new Date((Long) claims.get(CLAIM_KEY_CREATED));
+		} catch (Exception e) {
+			created = null;
+		}
+		return created;
+	}
 
-    public Date getCreatedDateFromToken(String token) {
-        Date created;
-        try {
-            final Claims claims = getClaimsFromToken(token);
-            created = new Date((Long) claims.get(CLAIM_KEY_CREATED));
-        } catch (Exception e) {
-            created = null;
-        }
-        return created;
-    }
+	public Date getExpirationDateFromToken(String token) {
+		Date expiration;
+		try {
+			final Claims claims = getClaimsFromToken(token);
+			expiration = claims.getExpiration();
+		} catch (Exception e) {
+			expiration = null;
+		}
+		return expiration;
+	}
 
-    public Date getExpirationDateFromToken(String token) {
-        Date expiration;
-        try {
-            final Claims claims = getClaimsFromToken(token);
-            expiration = claims.getExpiration();
-        } catch (Exception e) {
-            expiration = null;
-        }
-        return expiration;
-    }
+	private Claims getClaimsFromToken(String token) {
+		Claims claims;
+		try {
+			JwtParser jwtParser = Jwts.parser().setSigningKey(secret);
+			Jws<Claims> claimsJws = jwtParser.parseClaimsJws(token);
+			claims = claimsJws.getBody();
+		} catch (Exception e) {
+			claims = null;
+		}
+		return claims;
+	}
 
-    private Claims getClaimsFromToken(String token) {
-        Claims claims;
-        try {
-            JwtParser jwtParser = Jwts.parser().setSigningKey(secret);
-            Jws<Claims> claimsJws = jwtParser.parseClaimsJws(token);
-            claims = claimsJws.getBody();
-        } catch (Exception e) {
-            claims = null;
-        }
-        return claims;
-    }
+	private Date generateExpirationDate() {
 
-    private Date generateExpirationDate() {
-        return DateUtils.addDay(new Date(), expirationDay);
-    }
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(new Date());
+		cal.add(5, expirationDay);
+		return cal.getTime();
 
-    private Boolean isTokenExpired(String token) {
-        final Date expiration = getExpirationDateFromToken(token);
-        logger.info(
-                "JwtTokenUtil isTokenExpired expirationDay = " + expiration);
-        return expiration.before(new Date());
-    }
+	}
 
-    private Boolean isCreatedBeforeLastPasswordReset(Date created,
-                                                     Date lastPasswordReset) {
-        logger.info("JwtTokenUtil isTokenExpired created = " + created
-                + ", lastPasswordReset=" + lastPasswordReset);
-        return (lastPasswordReset != null && created.before(lastPasswordReset));
-    }
+	private Boolean isTokenExpired(String token) {
+		final Date expiration = getExpirationDateFromToken(token);
+		logger.info(
+				"JwtTokenUtil isTokenExpired expirationDay = " + expiration);
+		return expiration.before(new Date());
+	}
 
-    public String generateToken(UserDetails userDetails) {
-        Map<String, Object> claims = new HashMap<>();
-        claims.put(CLAIM_KEY_USER_ID, userDetails.getUsername());
-        claims.put(CLAIM_KEY_CREATED, new Date());
-        return generateToken(claims);
-    }
+	private Boolean isCreatedBeforeLastPasswordReset(Date created,
+			Date lastPasswordReset) {
+		logger.info("JwtTokenUtil isTokenExpired created = " + created
+				+ ", lastPasswordReset=" + lastPasswordReset);
+		return (lastPasswordReset != null && created.before(lastPasswordReset));
+	}
 
-    String generateToken(Map<String, Object> claims) {
-        return Jwts.builder().setClaims(claims)
-                .setExpiration(generateExpirationDate())
-                .signWith(SignatureAlgorithm.HS512, secret).compact();
-    }
+	public String generateToken(UserDetails userDetails) {
+		Map<String, Object> claims = new HashMap<>();
+		claims.put(CLAIM_KEY_USER_ID, userDetails.getUsername());
+		claims.put(CLAIM_KEY_CREATED, new Date());
+		return generateToken(claims);
+	}
 
-    public Boolean canTokenBeRefreshed(String token, Date lastPasswordReset) {
-        final Date created = getCreatedDateFromToken(token);
-        return !isCreatedBeforeLastPasswordReset(created, lastPasswordReset)
-                && !isTokenExpired(token);
-    }
+	String generateToken(Map<String, Object> claims) {
+		return Jwts.builder().setClaims(claims)
+				.setExpiration(generateExpirationDate())
+				.signWith(SignatureAlgorithm.HS512, secret).compact();
+	}
 
-    public String refreshToken(String token) {
-        String refreshedToken;
-        try {
-            final Claims claims = getClaimsFromToken(token);
-            claims.put(CLAIM_KEY_CREATED, new Date());
-            refreshedToken = generateToken(claims);
-        } catch (Exception e) {
-            refreshedToken = null;
-        }
-        return refreshedToken;
-    }
+	public Boolean canTokenBeRefreshed(String token, Date lastPasswordReset) {
+		final Date created = getCreatedDateFromToken(token);
+		return !isCreatedBeforeLastPasswordReset(created, lastPasswordReset)
+				&& !isTokenExpired(token);
+	}
 
-    public Boolean validateToken(String token, UserDetails userDetails) {
-        final String userId = getUsernameFromToken(token);
-        final Date created = getCreatedDateFromToken(token);
+	public String refreshToken(String token) {
+		String refreshedToken;
+		try {
+			final Claims claims = getClaimsFromToken(token);
+			claims.put(CLAIM_KEY_CREATED, new Date());
+			refreshedToken = generateToken(claims);
+		} catch (Exception e) {
+			refreshedToken = null;
+		}
+		return refreshedToken;
+	}
 
-        logger.info("JwtTokenUtil validateToken userId = " + userId
-                + ", created =" + created);
+	public Boolean validateToken(String token, UserDetails userDetails) {
+		final String userId = getUsernameFromToken(token);
+		final Date created = getCreatedDateFromToken(token);
 
-        boolean userIdEqual = userId.equals(userDetails.getUsername());
-        boolean isTokenExpired = isTokenExpired(token);
-        logger.info("JwtTokenUtil validateToken userIdEqual = " + userIdEqual
-                + ", isTokenExpired =" + isTokenExpired);
-        return (userIdEqual && !isTokenExpired);
-    }
+		logger.info("JwtTokenUtil validateToken userId = " + userId
+				+ ", created =" + created);
 
-    public static String string2Unicode(String string) {
-        StringBuffer unicode = new StringBuffer();
-        for (int i = 0; i < string.length(); i++) {
-            // 取出每一个字符
-            char c = string.charAt(i);
-            // 转换为unicode
-            unicode.append("\\u" + Integer.toHexString(c));
-        }
-        return unicode.toString();
-    }
+		boolean userIdEqual = userId.equals(userDetails.getUsername());
+		boolean isTokenExpired = isTokenExpired(token);
+		logger.info("JwtTokenUtil validateToken userIdEqual = " + userIdEqual
+				+ ", isTokenExpired =" + isTokenExpired);
+		return (userIdEqual && !isTokenExpired);
+	}
+
+	public static String string2Unicode(String string) {
+		StringBuffer unicode = new StringBuffer();
+		for (int i = 0; i < string.length(); i++) {
+			// 取出每一个字符
+			char c = string.charAt(i);
+			// 转换为unicode
+			unicode.append("\\u" + Integer.toHexString(c));
+		}
+		return unicode.toString();
+	}
 }
